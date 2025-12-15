1222c4252fa71cf27e141a7b137ec6f5b01feecf
radikru96 <radikru96@gmail.com>
2025-12-15 22:53:14 +0300

    Lab2 equalisation edited

--- Lab_2/equalisation.py
+++ Lab_2/equalisation.py
@@ -8,16 +8,22 @@ def equalisation(a: float, b: float):
 
     return -b/a
 
-data = [ [2,3]
-        ,[0,4]
-        ,[5,0]
-        ,[0,0] ]
+A = int(input("Введите число a "))
+B = int(input("Введите число b "))
 
-for t in data:
-    try: t.append( equalisation(*t) )
-    except Exception as e:
-        t.append( e )
+print(A, B, equalisation(A, B), sep="\t")
 
-print("a\tb\tx")
-for t in data:
-    print(*t,sep='\t')
+
+# data = [ [2,3]
+#         ,[0,4]
+#         ,[5,0]
+#         ,[0,0] ]
+
+# for t in data:
+#     try: t.append( equalisation(*t) )
+#     except Exception as e:
+#         t.append( e )
+
+# print("a\tb\tx")
+# for t in data:
+#     print(*t,sep='\t')
