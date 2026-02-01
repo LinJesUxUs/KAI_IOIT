09384e931964f90c9ebc3fd14f09f0db724bb05e
radikru96 <radikru96@gmail.com>
2026-02-01 19:52:53 +0300

    update 1

--- Lab_2/1.py
+++ Lab_2/1.py
@@ -1,29 +1,13 @@
 def equalisation(a: float, b: float):
     if (a == 0) and (b == 0):
-        raise ValueError("'x' равен любому числу")
+        return "'x' равен любому числу"
     if (b == 0):
         return float(0)
     if (a == 0):
-        raise ValueError("Нет решения.(деление на 0)")
-
+        return "Нет решения.(деление на 0)"
     return -b/a
 
 A = int(input("Введите число a "))
 B = int(input("Введите число b "))
 
 print(A, B, equalisation(A, B), sep="\t")
-
-
-# data = [ [2,3]
-#         ,[0,4]
-#         ,[5,0]
-#         ,[0,0] ]
-
-# for t in data:
-#     try: t.append( equalisation(*t) )
-#     except Exception as e:
-#         t.append( e )
-
-# print("a\tb\tx")
-# for t in data:
-#     print(*t,sep='\t')
